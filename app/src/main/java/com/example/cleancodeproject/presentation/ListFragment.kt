0ed49a3572a950a40f9dc95731986f9e9578226c
Tamires Pereira <tamirespereira6@gmail.com>
2021-01-14 16:27:09 -0300

    navigation

--- app/src/main/java/com/example/cleancodeproject/presentation/ListFragment.kt
+++ app/src/main/java/com/example/cleancodeproject/presentation/ListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.example.cleancodeproject.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +16,18 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        add_note.setOnClickListener {
+            goToNoteDetails()
+        }
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action: NavDirections = ListFragmentDirections.actionGoToNote()
+        Navigation.findNavController(notes_list_view).navigate(action)
     }
 }
--- app/src/main/java/com/example/cleancodeproject/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/cleancodeproject/presentation/NoteFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.cleancodeproject.R
+import kotlinx.android.synthetic.main.fragment_note.*
 
 class NoteFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,13 @@ class NoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        check_btn.setOnClickListener {
+            Navigation.findNavController(it).popBackStack()
+        }
     }
 }
