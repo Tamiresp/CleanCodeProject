53f4e7565ff9dcb47f14d4647e9701c5a2980c84
Tamires Pereira <tamirespereira6@gmail.com>
2021-01-18 12:30:51 -0300

    Showing list

--- app/src/main/java/com/example/cleancodeproject/presentation/ListFragment.kt
+++ app/src/main/java/com/example/cleancodeproject/presentation/ListFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cleancodeproject.R
+import com.example.cleancodeproject.framework.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    private val notesListAdapter = NoteListAdapter(arrayListOf())
+    private lateinit var viewModel: ListViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,9 +30,31 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        notes_list_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = notesListAdapter
+        }
+
         add_note.setOnClickListener {
             goToNoteDetails()
         }
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.notes.observe(this, Observer { notesList ->
+            loading_view.visibility = View.GONE
+            notes_list_view.visibility = View.VISIBLE
+            notesListAdapter.updateNotes(notesList.sortedByDescending { it.updateTime })
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getNotes()
     }
 
     private fun goToNoteDetails(id: Long = 0L) {
---
+++ app/src/main/java/com/example/cleancodeproject/presentation/NoteListAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.cleancodeproject.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cleancodeproject.R
+import com.example.core.data.Note
+import kotlinx.android.synthetic.main.item_note.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class NoteListAdapter(var notes: ArrayList<Note>): RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>() {
+
+    fun updateNotes(newNotes: List<Note>) {
+        notes.clear()
+        notes.addAll(newNotes)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NoteViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+       holder.bind(notes[position])
+    }
+
+    override fun getItemCount() = notes.size
+
+    inner class NoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val layout = view.noteLayout
+        private val noteTitle = view.title
+        private val noteContent = view.content
+        private val noteDate = view.date
+
+        fun bind(note: Note) {
+            noteTitle.text = note.title
+            noteContent.text = note.content
+
+            val sdf = SimpleDateFormat("MMM dd, HH:mm:ss")
+            val resultDate = Date(note.updateTime)
+            noteDate.text = "Last Updated: ${sdf.format(resultDate)}"
+        }
+    }
+}
